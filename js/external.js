ed65a7478189a1b29d965d8c1230e950bae11229
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-08-12 14:41:46 -0500

    complete: followed along with lecture

---
+++ js/external.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Hello from External JS!")
+var myStr = true;
+
+// console.log(myStr)
+
+
+// alert("Hey, this is definitely the external file.")
+// var userConfirm = confirm("Do you want to remain logged in?")
+
+// alter("The user selected: " + userConfirm)
+
+var userLunch = prompt (message: "What did you have for lunch?")
+
+alert ("The user had " + userLunch + "for lunch!")
