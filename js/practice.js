1be3e2955aa69c33e6228fbeec417010f98a6743
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-08-15 13:40:59 -0500

    practice file

---
+++ js/practice.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log("This file is to practice my coding!");
+
+//alert ("You should practice your coding!");
+
+//var practice = confirm("Are you sure you want to practice?");
+//console.log(practice);
+
+//var userInput = prompt ("Are you really practicing?");
+//console.log (userInput);
+
+
+function returnTwo(){
+    return 2
+}
+
+console.log("returnTwo()");//using "", returned "string"
+console.log(returnTwo());//no quotes returns actual "return"
+
+function sayHowdy(){
+    return "Howdy!"
+}
+
+console.log(sayHowdy());
+
