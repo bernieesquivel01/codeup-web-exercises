6ee4dcb8197deda2797fa7ccb2204277618fe5fb
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-08-16 09:44:26 -0500

    external JS exercises

--- js/external.js
+++ js/external.js
@@ -21,19 +21,28 @@ var userInput = prompt ("What's your favorite color?");
 alert(" Great, " + userInput + " is my favorite color too!");
 
 //Exercise 3 JavaScript with HTML
+//Kenneth's example:
+//var LittleMermaid = 3;
+//var brotherBear = 5;
+//var Hercules = 1;
+
+//var pricePerDay = 3;
+
+//console.log ("Hey you owe : $" + (LittleMermaid*priceperDay) + "for renting the Little Mermaid!")
 
 var LittleMermaid = prompt ("How many days have you had the Little Mermaid?");
-alert("Awesome! You owe: " + (LittleMermaid * 3));
+alert("Awesome! You owe: $" + (LittleMermaid * 3));
+//console.log(LittleMermaid + " is how long they had Little Mermaid!")//To show the number of days movie was rented in console.
 
 var BrotherBear = prompt ("How many days have you had BrotherBear?");
-alert("Great! That'll be: " + (BrotherBear * 3));
+alert("Great! That'll be: $" + (BrotherBear * 3));
 
 var Hercules = prompt ("How many days have you had Hercules?");
-alert("That'll be: " + (Hercules * 3));
+alert("That'll be: $" + (Hercules * 3));
 
 
 //A prompt: will return a string. . either: a. we need to PARSE the string into a number OR we could multiply all of our variables furst
-alert("Your total for all three movies is: " + (LittleMermaid*3+BrotherBear*3+Hercules*3));
+alert("Your total for all three movies is: $" + (LittleMermaid*3+BrotherBear*3+Hercules*3));
 
 var Google = 400
 var Amazon = 380
@@ -51,4 +60,18 @@ alert("You made " +FacebookPaycheck*Facebook+ " at Facebook!");
 alert ("Your total pay for the week is: " + (GooglePaycheck*Google+AmazonPaycheck*Amazon+FacebookPaycheck*Facebook));
 
 
+//productQuantity > 2 || !offerExpired
+//premiumMem === true, offer still needs to be valid
+
+//var productQuantity = 2
+//var offerValid = false;
+//var isPremiumMem = true;
+
+//console.log("Customer qualifies for offer? " + ((productQuantity > 2 || isPremiumMem) && offerValid));
+
+var productQuantity = prompt ("How many items are in your cart?")
+var offerValid = confirm ("Did you have a discount offer going on today?");
+var isPremiumMem = confirm ("Do you have your premium membership card?");
+
+alert("Customer qualifies for offer? " + ((productQuantity > 2 || isPremiumMem) && offerValid));
 
