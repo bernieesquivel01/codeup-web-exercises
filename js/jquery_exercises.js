98236cfbd2ee8bfba82379cfb8a0268f009c7d2b
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-09-28 12:27:58 -0500

    jquery-lecture

---
+++ js/jquery_exercises.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// $(document).ready(function() {
+//
+//     alert ("Document ready - Javascript is ready to rock and roll!");
+// });
+
+$(document).ready(function(){
+    alert( "The DOM has finished loading!" );
+});
