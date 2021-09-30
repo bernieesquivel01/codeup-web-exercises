f99883818136539a05f228d3d24f66f98898b892
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-09-30 09:41:42 -0500

    konami exercise

---
+++ js/konami.js
@@ -0,0 +1,29 @@
+
+//
+// var allowedKeys = [3838404037393739666513]
+//
+// var konamiCode = [3838404037393739666513];
+//
+// var konamiCodePosition = 0;
+//
+// document.addEventListener("keyup", function (e){
+//     if (allowedKeys === konamiCode){
+//         konamiCodePosition++;
+//
+//         if (konamiCodePosition === konamiCode.length) {
+//             activateCheats ();
+//             konamiCodePosition = 0;
+//         }
+//     } else {
+//         konamiCodePosition = 0;
+//     }
+// });
+//
+// function activateCheats() {
+//     // document.body.style.backgroundImage =
+//     //
+//     // var audio = new Audio ('audio/pling.mp3');
+//     // audio.play();
+//
+//     alert("You have added 30 lives!");
+// }
