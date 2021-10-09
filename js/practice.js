fb5f56408cc48ad26ff4cabe9bc49cc187de8ae5
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-10-09 18:50:14 -0500

    map search bar

--- js/practice.js
+++ js/practice.js
@@ -16,14 +16,71 @@
 
 //var number = Math.floor(Math.random() * 6) + 1;
 
-function rollSixSided(num){
+// function rollSixSided(num){
+//
+//     for(var i = 0; i < num; i++) {
+//
+//         var random = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+//
+//         console.log("You rolled a " + random + " on your dice!")
+//     }
+// }
+//
+//
+// rollSixSided(10);
 
-    for(var i = 0; i < num; i++) {
 
-        var random = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+// A warmup that might take a little research:
+//
+// So we've seen this dt property come up with some numbers in our work yesterday -
+//
+//     "dt": 1618317040
+//
+// Would you be able to write me a function named convertDateTime() - convertDateTime will take in a single num parameter along the lines of the property above []
+//
+// convertDateTime will return a form with human readable meaning - a date with a distinct month/year and perhaps other information.
 
-        console.log("You rolled a " + random + " on your dice!")
-    }
+function convertDateTime(num) {
+    var a = new Date(num * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var date = a.getDate();
+    var month = months[a.getMonth()]
+    var year = a.getFullYear()
+    var time = date + ' ' + month + ' ' + year;
+
+
+    return time;
+
 }
 
+console.log(convertDateTime(1633701944));
 
+//TA Example:
+
+var thisMorning = 1633702800;
+
+function convertDateTime (dtNum){
+    var dtinMs = dtNum * 1000; converting to milliseconds
+
+    var dateObj = new Date(dtinMs);
+
+    // return dateObj
+
+    return dateObj.toLocaleString()
+}
+
+convertDateTime(1633701944);
+
+// function timeConverter(UNIX_timestamp){
+//     var a = new Date(UNIX_timestamp * 1000);
+//     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//     var year = a.getFullYear();
+//     var month = months[a.getMonth()];
+//     var date = a.getDate();
+//     var hour = a.getHours();
+//     var min = a.getMinutes();
+//     var sec = a.getSeconds();
+//     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+//     return time;
+// }
+// console.log(timeConverter(0));
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,13 +1,6 @@
 <!-- OpenWeatherMap API -->
-//
-// $.get("https://api.openweathermap.org/data/2.5/onecall", {
-//     APPID: openWeatherKey, //API key variable, from keys.js
-//     lat: 29.423017,
-//     lon: -98.4853,
-//     units: "imperial"
-// }).get(dailyWeather[0]) {
-//     console.log(dailyWeather[0]);
 
+//Five Day Weather Info:
 $.get("https://api.openweathermap.org/data/2.5/onecall", {
     APPID: openWeatherKey, //API key variable, from keys.js
     lat: 29.423017,
@@ -18,24 +11,79 @@ $.get("https://api.openweathermap.org/data/2.5/onecall", {
     // console.log(data.daily[4].temp.day);
     var dailyWeather = "";
 
-    for (i = 1; i < 5; i++){
+//Time Converter:
+function timeConverter(dtNum){
+    var a = new Date(dtNum * 1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var day = days[a.getDay()]
+    var date = a.getDate();
+//     var hour = a.getHours();
+//     var min = a.getMinutes();
+//     var sec = a.getSeconds();
+    var time = day + ' ' + '<br>' + date;
+    console.log(time);
+    return time;
+}
+
+function hourlyConverter (dtNum) {
+    var a = new Date(dtNum * 1000);
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var hourlytime = hour + ' ' + min + ' ' + sec;
+    console.log(hourlytime)
+
+    return hourlytime
+}
+// console.log(timeConverter(1633716000));
+
+// var timeReturn = timeConverter(1633716000);
+    // console.log(data.daily[i].temp.day);
+    // console.log(timeReturn);
+    // var timeReturn = timeConverter(data.daily[i].dt);
+    // console.log(data)
+    // console.log(data.hourly[0])
+    // console.log(data.hourly[0].dt)
 
 
-    dailyWeather += "<div>";
-    dailyWeather += "Temperature: " + data.daily[i].temp.day + "<br>";
-    dailyWeather += "Feels like: " + data.daily[i].feels_like.day + "<br>";
-    dailyWeather += "Humidity: " + data.daily[i].humidity + "<br>";
-    dailyWeather += "Wind: " + data.daily[i].wind_speed + "<br>";
-    dailyWeather += "UV Index: " + data.daily[i].uvi + "<br>";
 
-    dailyWeather += "</div>";
 
+
+    for (i = 0; i < 6; i++) {
+        if (i === 0) {
+            for(j = 0; j < 6; j++) {
+                $("#day" + [i]).append("<b>"
+                    + hourlyConverter(data.hourly[j].dt)
+                    + "</b>"
+                    + "<br>").append("Temp: "
+                    + data.hourly[j].temp);
+            }
+        } else {
+            $("#day" + [i]).append("<b>"
+                + timeConverter(data.daily[i].dt)
+                + "</b>" + "<br>").append("Temp: "
+                + data.daily[i].temp.day);
+        }
     }
+
+    //     dailyWeather += "<div>";
+    // // dailyWeather += timeConverter(dtNum).daily[0].dt.day;
+    // dailyWeather += "Temp: " + "<br>" + data.daily[i].temp.day;
+    // // dailyWeather += "Feels like: " + data.daily[i].feels_like.day + "<br>";
+    // // dailyWeather += "Humidity: " + data.daily[i].humidity + "<br>";
+    // // dailyWeather += "Wind: " + data.daily[i].wind_speed + "<br>";
+    // // dailyWeather += "UV Index: " + data.daily[i].uvi + "<br>";
+    //
+    // dailyWeather += "</div>";
+
+
     console.log(dailyWeather);
 
-    $("#weeklyWeather").append(dailyWeather);
+    // $("#dailyWeather").append(dailyWeather);
 });
-
 
 <!-- Created Map -->
 
@@ -56,6 +104,25 @@ const marker = new mapboxgl.Marker({color: "black"})
     .setLngLat([-98.4895, 29.4267])
     .setPopup()
     .addTo(saWeatherMap)
+
+
+// map.addControl(new MapboxGeocoder({
+//     accessToken: mapboxgl.accessToken,
+//     bbox: [-74.390249, 40.414685, -73.519390, 40.976805] ,
+//     zoom: 15,}));
+
+// const geocoder = new MapboxGeocoder({
+//     // Initialize the geocoder
+//     accessToken: mapboxgl.accessToken, // Set the access token
+//     mapboxgl: mapboxgl, // Set the mapbox-gl instance
+//     marker: false, // Do not use the default marker style
+//     placeholder: 'Search for places in Berkeley', // Placeholder text for the search bar
+//     bbox: [-122.30937, 37.84214, -122.23715, 37.89838], // Boundary for Berkeley
+//     proximity: {
+//         longitude: -122.25948,
+//         latitude: 37.87221
+//     } // Coordinates of UC Berkeley
+// });
 
 // <!-- Adding Weather Icons -->
 // "weather": [
